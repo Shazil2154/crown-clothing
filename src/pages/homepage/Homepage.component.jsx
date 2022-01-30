1023ab978f5b70cc9804e95b969e187aedec0b55
Shazil2154 <sattarshazil@gmail.com>
2022-01-30 19:12:54 +0500

    create few components

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./Hompage.style.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
